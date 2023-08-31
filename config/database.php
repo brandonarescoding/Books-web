067799dbcd674ae88a41b22755ccf7f3b28e80ca
kamga <tamwabrandon8@email.com>
2023-09-01 01:59:32 +0200

    configuration de base de l'application

--- config/database.php
+++ config/database.php
@@ -1,1 +1,23 @@
 // database.php 
+
+<?php 
+// configuration de la base de donnee
+
+$dbHost = 'localhost';
+$dbname = 'webBooks';
+$dbUser = 'root';
+$dbPassword = '';
+
+try {
+
+    $pdo = new PDO ("mysql:host=$dbHost; dbname=$dbname", $dbUser, $dbPassword);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+
+    die("Erreur de connexion a la base de donnees: " . $e->getMessage());
+
+}
+
+?>
--- public/index.php
+++ public/index.php
@@ -1,2 +1,27 @@
 // index.php
 // index.php 
+
+<?php
+
+require_once './config/database.php'; // Inclure la bibliotheque de la base de donne
+
+// Inclure les controllers et les modeles si necessaires
+
+// Recuperer la page demandee (via GET)
+$page1 = isset($_GET['page1']) ? $_GET['page1'] : 'accueil';
+
+switch ($page1) {
+    case '/' :
+        //Inclure la vue da la page d'accueil
+        require 'ressources/views/home.php';
+        break;
+    
+    default:
+        //Inclure une vue par defaut (par exemple, une page d'erreur)
+        require 'ressources/views/404.php';
+        break;
+
+
+}
+
+?>
