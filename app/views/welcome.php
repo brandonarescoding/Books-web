7d786139b9f4df9577e62db42489afba74cbccd2
kamga <tamwabrandon8@email.com>
2023-09-02 14:36:41 +0200

    congig basic display

--- app/views/welcome.php
+++ app/views/welcome.php
@@ -4,18 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $pageTitle; ?></title>
-    <link rel="stylesheet" href="/public/css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 </head>
 <body>
-    <?php include_once 'resources/views/layout/header.php'; ?>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Mon Application</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Connexion</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Inscription</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Tableau de bord</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 
-    <div class="container">
-        <h1><?php echo $pageTitle; ?></h1>
-        <p><?php echo $welcomeMessage; ?></p>
-        <p>C'est la page d'accueil de notre application PHP.</p>
-        <p>Vous pouvez commencer à explorer nos fonctionnalités dès maintenant.</p>
+    <div class="container mt-5">
+        <div class="jumbotron">
+            <h1 class="display-4"><?php echo $pageTitle; ?></h1>
+            <p class="lead"><?php echo $welcomeMessage; ?></p>
+            <hr class="my-4">
+            <p>C'est la page d'accueil de notre application PHP.</p>
+            <p>Vous pouvez commencer à explorer nos fonctionnalités dès maintenant.</p>
+        </div>
     </div>
 
-    <?php include_once 'resources/views/layout/footer.php'; ?>
+    <footer class="bg-light text-center py-3">
+        © <?php echo date("Y"); ?> Mon Application
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
